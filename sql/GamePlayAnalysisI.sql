d71087b33a58387f8bc4167b5cc8e17d6c030b70
dcnis <bgk.dennis@yahoo.de>
2022-02-07 13:15:06 +0100

    added schema

--- sql/GamePlayAnalysisI.sql
+++ sql/GamePlayAnalysisI.sql
@@ -1,5 +1,18 @@
-
 /*
+CREATE table Activity (
+player_id int,
+device_id int,
+event_date date,
+games_played int
+);
+
+INSERT INTO Activity VALUES (1,2,'2016-03-01',5);
+INSERT INTO Activity VALUES (1,2,'2016-05-02',6);
+INSERT INTO Activity VALUES (2,3,'2017-06-25 ',1);
+INSERT INTO Activity VALUES (3,1,'2016-03-02',0);
+INSERT INTO Activity VALUES (3,4,'2018-07-03',5);
+
+
 Table: Activity
 
 +--------------+---------+
