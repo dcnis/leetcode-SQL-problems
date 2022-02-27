2e782bb60473af35ab7c3e1c25eb418564753af5
dcnis <bgk.dennis@yahoo.de>
2022-02-27 18:31:07 +0800

    added ProjectEmployeeII.sql

--- sql/Easy/ProjectEmployeeII.sql
+++ sql/Easy/ProjectEmployeeII.sql
@@ -6,3 +6,4 @@ https://leetcode.com/problems/project-employees-ii/
 select project_id from Project p
 GROUP BY project_id
 HAVING COUNT(employee_id) = (select COUNT(*) as c from Project GROUP BY project_id ORDER BY c desc limit 1);
+
