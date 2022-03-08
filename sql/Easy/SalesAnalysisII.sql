63a06c23766e5489c03fd3343e83cc5d8f4839a8
dcnis <bgk.dennis@yahoo.de>
2022-03-08 10:10:33 +0800

    added SalesAnalysisII.sql problem

--- sql/Easy/SalesAnalysisII.sql
+++ sql/Easy/SalesAnalysisII.sql
@@ -3,6 +3,7 @@
 https://leetcode.com/problems/sales-analysis-ii/
 */
 
+-- First solution --
 select distinct buyer_id from Sales
 WHERE buyer_id IN
 (select buyer_id from Sales
@@ -15,3 +16,23 @@ AND buyer_id NOT IN
 INNER JOIN Product USING(product_id)
 WHERE Product.product_name="iPhone");
 
+
+
+
+-- Second solution --
+select distinct buyer_id from Sales
+INNER JOIN Product USING(product_id)
+WHERE Product.product_name="S8"
+
+AND buyer_id NOT IN
+
+(select buyer_id from Sales
+INNER JOIN Product USING(product_id)
+WHERE Product.product_name="iPhone");
+
+
+-- Third solution --
+select buyer_id from Sales s
+INNER JOIN Product p USING (product_id)
+GROUP BY buyer_id
+HAVING SUM(p.product_name="iPhone")=0 AND SUM(p.product_name="S8")>0;
